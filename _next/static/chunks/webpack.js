6d1ebbfd652a89a7ccbccbc2033aa65070400235
Per Sunde <sunde.per92@gmail.com>
2021-01-14 19:29:29 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c38f6cb5155090d765c5";
+/******/ 	var hotCurrentHash = "7ed655027bb72fdb4488";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/pYWclbv7edvrP67ag69uE/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fprojects","\u002Fprojects\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
