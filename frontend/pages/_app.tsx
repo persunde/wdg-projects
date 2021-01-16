6ad587981ce5d78bc85e62ad9dc69e41a2ef555d
ghost <ghost@ghost>
2021-01-16 14:56:21 +0000

    basic house cleaning

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import 'normalize.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,37 +1,29 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Menu from '../components/menu/menu';
 
 export default function Home() {
-	return (
-		<div className="flex justify-center min-h-screen">
-			<Head>
-				<title>/wdg/ Projects</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+	return <>
+		<Head>
+			<title>/wdg/ - Web Dev General</title>
+			<link rel="icon" href="/favicon.ico"/>
+		</Head>
 
-			<main className="self-center text-center">
-				<h1 className="text-6xl">
-					Welcome to /wdg/ projects!
-				</h1>
-
-				<h3 className="">
-					Lets show off the cool projects people at /wdg/ are making!
-				</h3>
-				<Link href="/projects">
-					<a className="">
-						Click here to see a list of all the projects
-					</a>
-				</Link>
-
-				<div className="">
-					<div>
-						Our Github repository
-					</div>
-					<Link href="https://github.com/persunde/wdg-projects" >
-						<a target="_blank" rel="noopener noreferrer">https://github.com/persunde/wdg-projects</a>
-					</Link>
-				</div>
-			</main>
-		</div>
-	)
+		<main>
+			<h1>/wdg/ - Web Dev General</h1>
+			<Menu/>
+			<section>
+				<h2>Submitting a project update</h2>
+				<p>If you have a project you'd like to include an update for then make a post in the latest thread with the format of:</p>
+				<p>
+					:: my-project-title ::<br/>
+					dev:: anon<br/>
+					tools:: node, react, etc<br/>
+					link:: https://my.website.com<br/>
+					repo:: github.com/user/repo<br/>
+					progress:: sdgsghsghdsfgdfgsdf sfgsdfg sfgd sdg df gdsf g
+				</p>
+			</section>
+		</main>
+	</>
 }
