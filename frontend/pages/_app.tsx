888e394abfe1346eba3626eb5c2d660e9156dfb6
Per Sunde <sunde.per92@gmail.com>
2021-01-18 16:08:47 +0100

    added Umami OSS analytics

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,12 @@ import 'normalize.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <script async defer data-website-id="171e983d-1e4d-48ae-8f26-e11d63cc1567" src="https://analytics.nordictechjobs.com/umami.js"></script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
