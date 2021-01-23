fbc37585183a710d67ea67dd878680db1e8c31fa
Per Sunde <sunde.per92@gmail.com>
2021-01-23 13:50:06 +0100

    analytics is now only for prod

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,9 +2,14 @@ import 'normalize.css'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
+  let analytics = null
+  if (process.env.NODE_ENV === "production") {
+    analytics = <script async defer data-website-id="171e983d-1e4d-48ae-8f26-e11d63cc1567" src="https://analytics.nordictechjobs.com/umami.js"></script>
+  }
+  
   return (
     <>
-      <script async defer data-website-id="171e983d-1e4d-48ae-8f26-e11d63cc1567" src="https://analytics.nordictechjobs.com/umami.js"></script>
+      {analytics}
       <Component {...pageProps} />
     </>
   )
