fc2f74b9ce7eb6ff9ddb5a1a71874215ef26992a
Per Sunde <sunde.per92@gmail.com>
2021-01-30 23:56:02 +0100

    new ana id

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   let analytics = null
   if (process.env.NODE_ENV === "production") {
-    analytics = <script async defer data-website-id="171e983d-1e4d-48ae-8f26-e11d63cc1567" src="https://analytics.nordictechjobs.com/umami.js"></script>
+    analytics = <script async defer data-website-id="5da6576d-1382-444c-8aed-592906a467a8" src="https://analytics.nordictechjobs.com/umami.js"></script>
   }
   
   return (
