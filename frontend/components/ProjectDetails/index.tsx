8a8eddf9558a039ff5a167e1b9af46fb8e063c29
sadir <sadir@pm.me>
2021-02-03 23:41:47 +0100

    Fixed issue 16

--- frontend/components/ProjectDetails/index.tsx
+++ frontend/components/ProjectDetails/index.tsx
@@ -32,12 +32,29 @@ interface ProgressUpdateProps {
 	post: ProjectPost;
 }
 
+// Helper function for getDate
+const _leadingZero = (number) => {
+	return ("0" + number).slice(-2);
+}
+
+const getDate = (value) => {
+	const date = new Date(value);
+	const month = _leadingZero(date.getMonth() + 1);
+	const day = _leadingZero(date.getDate());
+	const year = date.getFullYear().toString().substr(-2);
+	const weekday = date.toLocaleDateString("en-US", { weekday: "short" });
+	const hours = _leadingZero(date.getHours());
+	const minutes = _leadingZero(date.getMinutes());
+	const seconds = _leadingZero(date.getSeconds());
+	return `${month}/${day}/${year}(${weekday})${hours}:${minutes}:${seconds}`;
+};
+
 const ProgressUpdate = ({ post }: ProgressUpdateProps) => {
 	const [imgExpanded, setImgExpanded] = useState(false);
 
 	return (
 		<div className="progress">
-			<div className="progress-head">Date: {post.updated_at}</div>
+			<div className="progress-head">Date: {getDate(post.updated_at)}</div>
 				{ post.image !== "" ? <div>
 					<img
 						className={styles.img}
