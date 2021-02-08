a2f40669911a945dcefaeb030a453cbcf99786af
ghost <ghost@ghost>
2021-02-08 20:35:11 +0000

    remove stray undefined

--- frontend/components/ProjectDetails/index.tsx
+++ frontend/components/ProjectDetails/index.tsx
@@ -39,7 +39,7 @@ interface ProgressUpdateProps {
 
 const ProgressUpdate = ({ post }: ProgressUpdateProps) => {
 	const [imgExpanded, setImgExpanded] = useState(false);
-	const dateStr = new Date(post.updated_at).toLocaleString(undefined);
+	const dateStr = new Date(post.updated_at).toLocaleString();
 	const img = post.image ? (
 		<img
 			className={styles.img}
