bf79e3cb891d4db3aaaa643cedbffa5957ba5258
bingsoo420 <gearmoss+bingsoo@gmail.com>
2023-09-21 00:05:26 +0100

    main: error checking

--- crawler/main.go
+++ crawler/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/persunde/wdg-projects/crawler/chanparser"
 	"github.com/persunde/wdg-projects/crawler/db"
 )
 
 func main() {
-	db.InitDatabaseTables()
+	err := db.InitDatabaseTables()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	postsWithProjectContent := chanparser.GetWDGProjectPosts()
 	if len(postsWithProjectContent) > 0 {
 		for _, post := range postsWithProjectContent {
-			db.InsertProjectPost(post)
+			err := db.InsertProjectPost(post)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 
 		// Somehow trigger the static website to be regenerated? Or should it be done by a different program/script?
